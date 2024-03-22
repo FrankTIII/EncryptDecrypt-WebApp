901b26f88bdcabba006702133e87664ed42f5509
Frank G <frank.training3@gmail.com>
2024-03-22 00:54:07 -0600

    Restore  Web and  js functionality.

--- scripts.js
+++ scripts.js
@@ -1,28 +1,48 @@
+function removeBackground() {
+    var textarea = document.getElementById('mensaje');
+    textarea.style.backgroundImage = 'none';
+  }
+
 function encrypt() {
-    const inputText = document.getElementById('inputText').value.toLowerCase();
+    const inputText = document.getElementById('input-text').value.toLowerCase();
     const encryptedText = inputText
         .replaceAll('e', 'enter')
         .replaceAll('i', 'imes')
         .replaceAll('a', 'ai')
         .replaceAll('o', 'ober')
         .replaceAll('u', 'ufat');
-    document.getElementById('outputText').textContent = encryptedText;
+    removeBackground();
+    document.getElementById('mensaje').textContent = encryptedText;
 }
 
 function decrypt() {
-    const inputText = document.getElementById('inputText').value.toLowerCase();
+    const inputText = document.getElementById('input-text').value.toLowerCase();
     const decryptedText = inputText
         .replaceAll('enter', 'e')
         .replaceAll('imes', 'i')
         .replaceAll('ai', 'a')
         .replaceAll('ober', 'o')
         .replaceAll('ufat', 'u');
-    document.getElementById('outputText').textContent = decryptedText;
+    document.getElementById('mensaje').textContent = decryptedText;
 }
 
 function copyToClipboard() {
-    const outputText = document.getElementById('outputText').textContent;
+    const outputText = document.getElementById('mensaje').textContent;
     navigator.clipboard.writeText(outputText)
-        .then(() => alert('Texto copiado al portapapeles'))
+        .then(() => console.log('Texto copiado al portapapeles'+outputText))
         .catch(err => console.error('Error al copiar al portapapeles:', err));
+
+
+        
 }
+
+
+/*let text = document.getElementById('outputText').textContent/*.innerHTML;
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(text);
+      console.log('Content copied to clipboard');
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+  }*/
